dcb06f0125d6d3dea098a3e66b47651029cd3442
avinashshrangee@gmail.com <avinashshrangee@gmail.com>
2025-01-06 23:40:40 +0530

    ForthDay

--- src/main/java/serial/Deserializer.java
+++ src/main/java/serial/Deserializer.java
@@ -1,108 +1,97 @@
 package serial;
 
+import type.KValue;
+import type.KValueType;
+
 import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 
-import type.KValue;
 
 public class Deserializer {
-	
-	private final DataInputStream inputStream;
-	private int messageSize;
 
-	public Deserializer(InputStream inputStream) {
-		// TODO Auto-generated constructor stub
-		this.inputStream = new DataInputStream(inputStream);
-	}
-	
-	
-	public KValue read() throws IOException{
-		
-		var value = new KValue();
-		
-		this.messageSize = inputStream.readInt();
-		System.out.println("messageSize : "+ messageSize);
-		
-		parseHeader(value);
-		
-		return switch(value.getType()) {
-			case ApiVersion->parserApiVersion(value);
-			case DescribeTopic -> parserDescribeTopic(value);
-			case Fetch ->null;
-			case Unknown ->value;
-		};
-		
-	}
+    private final DataInputStream inputStream;
+    private int messageSize;
 
-	private KValue parserApiVersion(KValue value) throws IOException {
-		// TODO Auto-generated method stub
-		inputStream.readNBytes(messageSize);
-		return value;
-	}
+    public Deserializer(InputStream inputStream) {
+        this.inputStream = new DataInputStream(inputStream);
+    }
 
-	private KValue parserDescribeTopic(KValue value) throws IOException {
-		// TODO Auto-generated method stub
-		final var arrayLength = readLength()-1;
-		
-		final var topicName = readString();
-	
-		inputStream.readByte();
-		
-		final var paritionLimit = inputStream.readInt();
-		inputStream.readByte();
-		inputStream.readByte();
-		
-		System.out.println("Topic :" + topicName);
-		
-		value.setTopic(topicName);
-		
-		return value;
-		
-	}
+    public KValue read() throws IOException {
+        var value = new KValue();
 
+        this.messageSize = inputStream.readInt();
+        System.out.println("messageSize = " + messageSize);
 
+        parseHeader(value);
 
+        return switch (value.getType()) {
+            case ApiVersion -> parseApiVersion(value);
+            case DescribeTopic -> parseDescribeTopic(value);
+            case Fetch -> null;
+            case Unknown -> value;
+        };
+    }
 
-	private String readString() throws IOException {
-		final var length = readLength()-1;
-		
-		final var content = inputStream.readNBytes(length);
-		
-		return new String(content , StandardCharsets.US_ASCII);
-	}
+    private KValue parseApiVersion(KValue value) throws IOException {
 
+        inputStream.readNBytes(messageSize);
 
-	private int readLength() throws IOException {
+        return value;
+    }
+
+    private KValue parseDescribeTopic(KValue value) throws IOException {
+        final var arrayLength = readLength() - 1;
+        final var topicName = readString();
+        
+        inputStream.readByte(); // skip tag buffer
+
+        final var partitionLimit = inputStream.readInt();
+
+//        System.out.println("array length = " + arrayLength);
+//        System.out.println("topicName = " + topicName);
+//        System.out.println("partitionLimit = " + partitionLimit);
+
+        inputStream.readByte(); // skip cursor
+        inputStream.readByte(); // skip tag buffer
+
+        value.setTopic(topicName);
+        
+        return value;
+    }
+
+    private void parseHeader(KValue value) throws IOException {
+        final int apiKey = inputStream.readShort(); // 2 bytes
+        final int apiVersion = inputStream.readShort(); // 2 bytes
+        final int correlationId = inputStream.readInt(); // 4 bytes
+        final int clientLength = inputStream.readShort(); // 2 bytes
+        final var clientId = inputStream.readNBytes(clientLength);
+        inputStream.readByte(); // skip tag buffer
+
+        messageSize = messageSize - 11 - clientLength;
+
+        value.setApiKey(apiKey);
+        value.setApiVersion(apiVersion);
+        value.setCorrelationId(correlationId);
+    }
+
+    private String readString() throws IOException {
+        final var length = readLength() - 1;
+
+        //System.out.println("length = " + length);
+
+        final var content = inputStream.readNBytes(length);
+        return new String(content, StandardCharsets.US_ASCII);
+    }
+
+    private int readUnsignedByte() throws IOException {
+        return Byte.toUnsignedInt(inputStream.readByte());
+    }
+
+    private int readLength() throws IOException {
         final var first = readUnsignedByte();
 
         return first & 0b0011_1111;
     }
-
-
-	private int readUnsignedByte()throws IOException {
-		// TODO Auto-generated method stub
-		return Byte.toUnsignedInt(inputStream.readByte());
-	}
-
-
-	private void parseHeader(KValue value) throws IOException {
-		// TODO Auto-generated method stub
-		final int apiKey = inputStream.readShort();
-		final int apiVersion = inputStream.readShort();
-		final int correlationId = inputStream.readInt();
-		final int clientLength = inputStream.readShort();
-		final var clientId = inputStream.readNBytes(clientLength);
-		inputStream.readByte();
-		
-		messageSize = messageSize - 11 - clientLength;
-		
-		value.setApiKey(apiKey);
-		value.setApiVersion(apiVersion);
-		value.setCorrelationId(correlationId);
-		
-		
-	}
-
 }
